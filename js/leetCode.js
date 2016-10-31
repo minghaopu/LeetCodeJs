eb3000b41089a761cdb45e4b39b64cd2e277fd1f
roneilPMH <roneil_PMH@hotmail.com>
2016-10-31 16:34:51 -0400

    Finish question 280; if (i is odd) nums[i] > nums[i - 1]; if (i is even) nums[i] < nums[i - 1];

--- js/leetCode.js
+++ js/leetCode.js
@@ -2056,6 +2056,24 @@
 				}
 			}
 			return false;
+		},
+		/*
+		 *
+		 *     280. Wiggle Sort
+		 *
+		 */
+		/**
+		 * @param {number[]} nums
+		 * @return {void} Do not return anything, modify nums in-place instead.
+		 */
+		wiggleSort: function(nums) {
+			for (var i = 1; i < nums.length; i++) {
+				if (((i & 1) && nums[i] < nums[i - 1]) || (!(i & 1) && nums[i] > nums[i - 1])) {
+					var t = nums[i];
+					nums[i] = nums[i - 1];
+					nums[i - 1] = t;
+				}
+			}
 		};
 
 	})
